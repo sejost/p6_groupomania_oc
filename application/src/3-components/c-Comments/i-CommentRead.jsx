06e5e46484a4273912e2d15c947497cf3f48f7a6
Sebastien Jost <jost.seb@gmail.com>
2022-10-13 16:36:11 +0200

    Format Comment Date

--- application/src/3-components/c-Comments/i-CommentRead.jsx
+++ application/src/3-components/c-Comments/i-CommentRead.jsx
@@ -1,20 +1,17 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState }  from 'react';
 import PropTypes from 'prop-types';
 
 import { formatDate } from '../b-Posts/x-PostFunctions';
 import useAuth from '../../1-hooks/useAuth';
 
 const CommentRead = (props) => {
-	const { auth } = useAuth();
-	const [postId, setPostId] = useState('');
 	const [changePending, setChangePending] = useState(false);
-	const authId = auth.userId;
 
 	return (
 		<>		
 			<div className="comment__headPart">
 				<h4 className="comment__ownerId">Commentaire de {props.commenterName}</h4>
-				<span className="comment__commentDate">le {props.commentDate}</span>
+				<span className="comment__commentDate">le {formatDate(props.commentDate, 'dd/mm/yy à hh:mn')}</span>
 			</div>
 			<div className="comment__maintPart">
 				<div className="comment__commentText">{props.commentText}</div>
